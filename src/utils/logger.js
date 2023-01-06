35a183bf1ec18ede988d1e7b03ebad35d5a81318
rasenshuriken <omerozoglu.1@gmail.com>
2023-01-06 15:02:06 +0300

    fix: Fix logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,15 +4,16 @@ require("winston-daily-rotate-file");
 
 const CATEGORY = "Blockchain API";
 
-//Using the printf format.
 const customFormat = printf(({ level, message, label, timestamp }) => {
   return `${timestamp} [${label}] ${level} ::  ${message}`;
 });
+
 const fileRotateTransport = new transports.DailyRotateFile({
   filename: "../../logs/rotate-%DATE%.log",
   datePattern: "YYYY-MM-DD",
   maxFiles: "14d",
 });
+
 const logger = createLogger({
   level: "debug",
   format: combine(label({ label: CATEGORY }), timestamp(), customFormat),
@@ -30,9 +31,4 @@ const logger = createLogger({
   ],
 });
 
-logger.log("debug", "Debug");
-logger.debug("Debug");
-logger.info("Info");
-logger.warn("Warn");
-throw new Error("awdwad");
 module.exports = logger;
